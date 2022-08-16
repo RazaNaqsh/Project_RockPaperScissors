d396c4e0e6a85fff824e65a0a51c982837818047
Raza <owaisraza1704@gmail.com>
2022-08-16 17:51:52 +0530

    Accept user input and Correct the case

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ function getComputerChoice(){
     return weapons[Math.floor(Math.random() * weapons.length)]
     }
 console.log(getComputerChoice());
+
+const computerSelection = getComputerChoice();
+const playerInput = prompt("Choose Your Weapon!");
+console.log(playerInput);
+
+//function for correcting case of input
+function caseCorrector(input){
+    return input[0].toUpperCase()+input.slice(1).toLowerCase()
+}
+
+console.log(caseCorrector(playerInput));
