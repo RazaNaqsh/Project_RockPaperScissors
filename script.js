1a60d1bbcb1d3a1d87fbbafb5252cd665d6b177f
Raza <owaisraza1704@gmail.com>
2022-08-16 17:30:31 +0530

    Define function to make computer play

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("check");
+//Function to make computer play
+
+function getComputerChoice(){
+    let weapons = ["Rock", "Paper", "Scissors"]
+    return weapons[Math.floor(Math.random() * weapons.length)]
+    }
+console.log(getComputerChoice());
